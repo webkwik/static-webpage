57e4f95f5e8cb2251ccb4a81901bdd90a724f33c
v_nakon <nakonechny.vl@hotmail.com>
2018-12-12 20:13:00 +0200

    added class for search

--- src/scripts.js
+++ src/scripts.js
@@ -7,15 +7,24 @@ function displayElement(form) {
 }
 
 function displayElementSearch(form, searchIcon) {
-    form.style.opacity = 1;
-    searchIcon.style.opacity = 0;
+    form.classList.add("for-form-search");
+    searchIcon.classList.add("for-icon-search");
+}
 
+function removeElementSearch(form, searchIcon) {
+    form.classList.remove("for-form-search");
+    searchIcon.classList.remove("for-icon-search");
 }
 
 var searchIcon = document.getElementById("search");
 var form = document.getElementById("form");
 searchIcon.addEventListener("click", function () {
     displayElementSearch(form, searchIcon)}
+    , false);
+
+var button = document.getElementById("button");
+button.addEventListener("click", function () {
+    removeElementSearch(form, searchIcon)}
     , false);
 
 var menu = document.getElementById("menu-toggle");
