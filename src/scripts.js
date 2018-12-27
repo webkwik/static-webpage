d0bee1e1d37d7d0753d2acfb7ab0c1ca02fc73d7
v_nakon <nakonechny.vl@hotmail.com>
2018-12-28 01:24:07 +0200

    add js for menu

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,3 @@
-function displayElement(form) {
-    if (form.style.display == "none") {
-        form.style.display = "block"
-    } else {
-        form.style.display = "none";
-    }
-}
-
 function displayElementSearch(form, searchIcon) {
     form.classList.add("for-form-search");
     searchIcon.classList.add("for-icon-search");
@@ -28,7 +20,23 @@ button.addEventListener("click", function () {
     , false);
 
 var menu = document.getElementById("menu-toggle");
-var toggle = document.getElementById("toggle");
+var containerToggle = document.getElementById("container-toggle");
 menu.addEventListener("click", function () {
-    displayElement(toggle)}
+    addClass(containerToggle, menu)}
+    , false);
+
+
+function addClass(containerToggle, menu) {
+    containerToggle.classList.add("open-menu");
+    menu.classList.add("visibility-icon-menu");
+}
+
+function removeClass(containerToggle, menu) {
+    containerToggle.classList.remove("open-menu");
+    menu.classList.remove("visibility-icon-menu");
+}
+
+var exitMenu = document.getElementById("exit-menu");
+exitMenu.addEventListener("click", function () {
+    removeClass(containerToggle, menu)}
     , false);
